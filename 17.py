36d3ab164c423ab7db8718f5575fb2987e3a6e22
Michael vaknin <71179246+michaelvaknin@users.noreply.github.com>
2022-08-11 18:57:17 +0300

    Test1 (#1)
    * add test1
    
    * Create README.md
    
    Co-authored-by: michaelvaknin <michaelvaknin30815@gmail.com>

--- 17.py
+++ 17.py
@@ -2,7 +2,10 @@ from arit import addition, sub
 from get_data import get_number
 import datetime
 def dec(function_to_run):
+    # my comment
+
     def wrapper():
+
         print(datetime.datetime.now())
         function_to_run()
         print(datetime.datetime.now())
